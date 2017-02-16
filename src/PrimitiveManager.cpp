6f96ceb2185156261d3718681499811807da2f09
eikur <jorge.soriano.pinedo@gmail.com>
2017-02-16 19:12:30 +0100

    Added files for ImGui integration

--- src/PrimitiveManager.cpp
+++ src/PrimitiveManager.cpp
@@ -20,12 +20,13 @@ PrimitiveManager::~PrimitiveManager()
 Primitive* PrimitiveManager::createPrimitive(Primitive::Types primitive_type)
 {
 	static_assert(Primitive::Types::UNKNOWN == 4, "Code needs update");
+
 	Primitive* ret = nullptr;
 	switch (primitive_type) {
-	case Primitive::Types::AXIS: ret = new Axis(); break;
-	case Primitive::Types::GRID: ret = new Grid(); break;
-	case Primitive::Types::SOLID_CUBE: ret = new SolidCube(); break;
-	case Primitive::Types::SOLID_SPHERE: ret = new SolidSphere(0.5f, 10, 10); break;
+		case Primitive::Types::AXIS: ret = new Axis(); break;
+		case Primitive::Types::GRID: ret = new Grid(); break;
+		case Primitive::Types::SOLID_CUBE: ret = new SolidCube(); break;
+		case Primitive::Types::SOLID_SPHERE: ret = new SolidSphere(0.5f, 10, 10); break;
 	}
 
 	if (ret != nullptr)
