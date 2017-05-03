e52983b05e36e78cbf6bda1052e8beb351b6c4fa
eikur <jorge.soriano.pinedo@gmail.com>
2017-05-03 23:11:07 +0200

    Created class for debug draw the physics world

---
+++ src/PhysicsDebugDrawer.cpp
@@ -0,0 +1,28 @@
+#include "Bullet/include/btBulletDynamicsCommon.h"
+#include "PhysicsDebugDrawer.h"
+
+
+PhysicsDebugDrawer::PhysicsDebugDrawer() {}
+
+void PhysicsDebugDrawer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
+{}
+
+void PhysicsDebugDrawer::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB,
+	btScalar distance, int lifeTime, const btVector3& color)
+{}
+
+void PhysicsDebugDrawer::reportErrorWarning(const char* warningString)
+{}
+
+void PhysicsDebugDrawer::draw3dText(const btVector3& location, const char* textString)
+{}
+
+void PhysicsDebugDrawer::setDebugMode(int debugMode)
+{
+	mode = (DebugDrawModes) debugMode; // test thoroughly
+}
+
+int PhysicsDebugDrawer::getDebugMode() const
+{
+	return mode; 
+}
---
+++ src/PhysicsDebugDrawer.h
@@ -0,0 +1,19 @@
+#ifndef __PHYSICSDEBUGDRAWER_H__
+#define __PHYSICSDEBUGDRAWER_H__
+
+class PhysicsDebugDrawer : public btIDebugDraw
+{
+public:
+	PhysicsDebugDrawer() {}
+	void drawLine(const btVector3& from, const btVector3& to, const btVector3& color);
+	void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB,
+		btScalar distance, int lifeTime, const btVector3& color);
+	void reportErrorWarning(const char* warningString);
+	void draw3dText(const btVector3& location, const char* textString);
+	void setDebugMode(int debugMode);
+	int getDebugMode() const;
+public:
+	DebugDrawModes mode;
+};
+
+#endif // !__PHYSICSDEBUGDRAWER_H__
