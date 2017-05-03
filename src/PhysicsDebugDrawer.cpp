a6608eda1e1c6ecf7289da0a6f8f50084ec5f1c8
eikur <jorge.soriano.pinedo@gmail.com>
2017-05-03 23:20:03 +0200

    Class PhysicsDebugDrawer: implemented drawLine () function

--- src/PhysicsDebugDrawer.cpp
+++ src/PhysicsDebugDrawer.cpp
@@ -1,11 +1,19 @@
 #include "Bullet/include/btBulletDynamicsCommon.h"
+#include "glew-2.0.0/include/GL/glew.h"
 #include "PhysicsDebugDrawer.h"
 
 
 PhysicsDebugDrawer::PhysicsDebugDrawer() {}
 
 void PhysicsDebugDrawer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
-{}
+{
+	// direct method
+	glBegin(GL_LINES); 
+	glColor3fv(color); 
+	glVertex3fv(from); 
+	glVertex3fv(to); 
+	glEnd(); 
+}
 
 void PhysicsDebugDrawer::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB,
 	btScalar distance, int lifeTime, const btVector3& color)
